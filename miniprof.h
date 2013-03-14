5b7ca0e37fda7269363ce8a01bb928c83eb69374
Renaud Lachaize <renaud.lachaize@inria.fr>
2013-03-14 11:35:51 +0100

    added/modified comments in miniprof.h

--- miniprof.h
+++ miniprof.h
@@ -59,17 +59,53 @@ do {                         \
          } while(0)
 
 typedef struct _event {
+   /* Useless in the current version (we only monitor "raw" events) */
    uint64_t type;
+   /* Value describing the chosen event and unitmask (see details below). */
    uint64_t config;
+   /* Boolean indicating if kernel-level events must be monitored */
    uint64_t exclude_kernel;
+   /* Boolean indicating if user-level events must be monitored */
    uint64_t exclude_user;
 
    const char* name;
    char per_node;
 
+   /* Id of the MSR control register that will be used to monitor the event */
    uint64_t msr_select; /* msr that will be used to monitor the event */
+   /* Id of the MSR counter register that will be used to monitor the event */
    uint64_t msr_value; /* msr that will be used to monitor the event */
 } event_t;
+
+/*
+ ***Additional details about event IDs for AMD 10h and 15h architectures***
+ * 
+ * The "config" field in a "event_t" struct must be set with values using the
+ * following scheme: 0xz0040yyzz
+ * (for details, see PERF_CTL description in AMD BKDG)
+ *
+ *  'z-zz' values: EventSelect 
+ *  'yy': UnitMask
+ *  '4': enable performance counter
+ *       (not strictly required because forced by miniprof anyways)
+ *
+ *  Examples for AMD 15h architectures (BKDG 15h section 3.15):
+ *     1) CPU clocks not halted (per-core event):
+ *             z-zz: 0x076
+ *             yy: 0x00 (no unitmask)
+ *     2) L2 cache misses (per core event) with unitmask selecting
+ *          "DC fill" + "TLB page table walk":
+ *             z-zz: 0x07E
+ *             yy: 0x03
+ *     3) Tagged IBS Ops (per core event) with unitmask selecting
+ *           "Number of ops tagged by IBS that retired"
+ *             z-zz: 0x1CF
+ *             yy: 0x01
+ *     4) DRAM accesses (per-die event) with unitmask selecting
+ *          "DCT1 Page hit":
+ *             z-zz: 0x0E0
+ *             yy: 0x08
+ */
 
 
 #ifdef __x86_64__
@@ -86,7 +122,7 @@ typedef struct pdata {
    int core;
    int nb_events;
    event_t *events;
-   int tid; /* Tid to observe */
+   int tid; /* Tid to observe (useless in current version?) */
 } pdata_t;
 
 #endif /* PROFILER_H_ */
